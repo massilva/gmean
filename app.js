7d367d8eff7027fff7039555592742c017b9ae67
Marcos Silva <chuckymass@gmail.com>
2015-08-06 22:42:15 -0300

    Adicionando API do youtube. Testando retorno de busca.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var search = require('./routes/search');
 var http = require('http');
 var path = require('path');
 
@@ -30,6 +31,8 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.post('/contato', routes.adicionaContato);
 app.get('/contatos', routes.listaContatos);
+app.get('/testSearch', search.index);
+app.get('/videos', search.resultList);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port: ' + app.get('port'));
---
+++ routes/search.js
@@ -0,0 +1,32 @@
+var Youtube = require("youtube-api")
+  , Http = require("http")
+  , Request = require("request")
+  , path = require("path")
+  ;
+
+var oauth = Youtube.authenticate({
+    type: "key"
+  , key: "AIzaSyAmz13xpZkwC15OzGBsDY32wc66h6jfFlM"
+});
+	
+var conexoes = 0;
+var videos = {};
+
+exports.index = function(req, res){
+	conexoes++;
+	console.log(conexoes);
+	var q = "MV Bill";
+	Youtube.search.list({part: 'id, snippet',q: q, maxResults: 10}, function(error, result) {
+		if (error) {
+			console.log(error);
+			res.render('error', { title: 'Testando busca ERROR'});
+		}else {
+			videos = result;
+			res.render('search', { q: q, title: 'Testando busca', videos : videos.items });
+		}
+	});
+};
+
+exports.resultList = function(req, res) {
+	res.json({ videos : videos }); 
+}
